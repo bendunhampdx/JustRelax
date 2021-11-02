ddb44d3305a6d6e160d5aea6de87eb8032b36237
chynnalew <chynnalew@yahoo.com>
2021-11-02 13:41:37 -0700

    initial styling

--- JustRelax/Program.cs
+++ JustRelax/Program.cs
@@ -1,24 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Media;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
 
 namespace JustRelax
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      
-      Program program = new Program();
-      
-      program.Intro();
-    }
-    public void Intro()
-    {
-      Console.WriteLine("Hello students! Welcome to  ~Spepicodus~ programming bootcamp!");
-      Console.WriteLine("My name is ~Spames~ and I will be your instructor for this course.");
-      Console.WriteLine("This bootcamp is designed to challenge those who have little to no experience with programming, so remember that it's alright to feel lost at times.");
-      Console.WriteLine("It can be hard to stay focused so I reccomend listening to background music as you code. This is my favorite coding music:");
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
   }
 }
---
+++ JustRelax/wwwroot/js/scripts.js
@@ -0,0 +1,24 @@
+﻿
+$(document).ready(function () {
+  $('p').prepend('> ');
+  $('#clickHereToBegin').click(function(){
+    $('#titleScreen').hide();
+    $('#disclaimer').show();
+  });
+  $('#continueToIntro').click(function(){
+    $('#disclaimer').fadeOut(1500);
+    $('#introScene').delay(1500).fadeIn(3000);
+    $('#pressEnter1').delay(4000).show();
+    $('#introSceneContinue').delay(5000).fadeIn(1000);
+  })
+  $('#introSceneContinue').click(function () {
+    console.log("hi");
+    var introDivsCount = 1;
+    var totalIntroDivsCount = 5
+    if(introDivsCount < totalIntroDivsCount)
+    {
+      introDivsCount += 1;
+      $("#introLine"+introDivsCount).show();
+    }
+  })
+});
